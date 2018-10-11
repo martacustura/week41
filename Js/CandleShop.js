3a54d5a43903aaf118cf9c8ff7c01779f2dd1a93
seelk89 <seelk.89@gmail.com>
2018-10-11 14:57:52 +0200

    1. commit

---
+++ Js/CandleShop.js
@@ -0,0 +1,121 @@
+//Getting candles
+function listCandles() {
+    // Call Web API to get a list of post
+    $.ajax({
+        url: 'http://candleshop.azurewebsites.net/api/Candles',
+        type: 'GET',
+        dataType: 'json',
+        success: function (customers) {
+            onGetCustomersSuccess(customers);
+        },
+        error: function (request, message, error) {
+            handleException(request, message, error);
+        }
+    });
+}
+
+function onGetCustomersSuccess(customers) {
+    if ($("#customersTable tbody").length == 0) {
+        $("#customersTable").append("<tbody></tbody>");
+    }
+    $("#customersTable tbody").empty();
+    // Iterate over the collection of data
+    $.each(customers, function (index, customer) {
+        // Add a row to the post table
+        addCustomerRow(customer);
+    });
+}
+
+function addCustomerRow(customer) {
+    // Check if <tbody> tag exists, add one if not
+    // Append row to <table>
+    $("#customersTable tbody").append(
+        buildCustomerRow(customer));
+}
+
+function buildCustomerRow(candle) {
+    var ret =
+        "<tr>" +
+        "<td>" + candle.id + "</td>" +
+        "<td>" + candle.name + "</td>" +
+        "<td>" + candle.type + "</td>" +
+        "<td>" + candle.price + "</td>" +
+        "<td>" + candle.stock + "</td>" +
+        "<td>" + candle.imageURL + "</td>" +
+        "<td>"
+        "<i class='fas fa-minus-circle'></i>" +
+        "</button>" +
+        "</td >" +
+        "</tr>";
+    return ret;
+}
+
+//Creating a candle
+$('#newCandleForm').on('submit',function(e){
+    e.preventDefault();
+    var name = $( "#candleName" ).val();
+    var type = $( "#candleType" ).val();
+    var price = $( "#candlePrice" ).val();
+    var stock = $("#candleStock" ).val();
+    var imageURL = $("#candleImageURL").val();
+
+    $.ajax({
+        url: 'http://candleshop.azurewebsites.net/api/Candles',
+        type: 'POST',
+        data: JSON.stringify({
+            "name": name,
+            "type": type,
+            "price": price,
+            "stock": stock,
+            "imageURL": imageURL}),
+        processData: false,
+        contentType: 'application/json',
+        success: function (comments) {
+            console.log("Created candle");
+        },
+        error: function (request, message, error) {
+            handleException(request, message, error);
+        }
+    });
+});
+
+//Updating candles
+ $("#put").click(function(){
+     var id = $("#updateCandleId").val();
+     var name = $( "#newCandleName" ).val();
+     var type = $( "#newCandleType" ).val();
+     var price = $( "#newCandlePrice" ).val();
+     var stock = $("#newCandleStock" ).val();
+     var imageURL = $("#newCandleImageURL").val();
+
+     var settings = {
+         url: "http://candleshop.azurewebsites.net/api/Candles/" + id,
+         type: "PUT",
+         contentType: 'application/json',
+         data: JSON.stringify({
+             id: id,
+             name: name,
+             type: type,
+             price: price,
+             stock: stock,
+             imageURL: imageURL})
+     };
+     $.ajax(settings);
+ })
+
+//Deleting candles
+$('#deleteCandleForm').on('submit',function(e){
+    e.preventDefault();
+    var id = $("#deleteCandleId").val();
+
+    $.ajax({
+        url: 'http://candleshop.azurewebsites.net/api/Candles/' + id,
+        type: 'DELETE',
+        success: function (comments) {
+            console.log("Deleted candle");
+        },
+        error: function (request, message, error) {
+            handleException(request, message, error);
+        }
+    });
+});
